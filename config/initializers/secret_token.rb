0693030a5fa5778990134d4101ca24a1219620d1
Edward Elliott <edelliott@nc.rr.com>
2012-07-27 07:16:03 -0400

    Initial commit of second try at app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BuyStandConsumer::Application.config.secret_token = '8b4bd4406defb8cc04bd3af95c619d4c4ae4cf06615f9021e5aecfad9be22fb96a50a1b8661f9a2a6b1d17025e7a46d7290508ef25cdda13033310383116f663'
