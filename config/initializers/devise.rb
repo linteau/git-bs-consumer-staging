23ffec88c058ac4d90eb7a1e53e802b7b969058c
Edward Elliott <edelliott@nc.rr.com>
2012-07-29 13:10:06 -0400

    Fixing devise.rb for Heroku

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -198,8 +198,9 @@ Devise.setup do |config|
 	# Add a new OmniAuth provider. Check the wiki for more information on setting
 	# up on your models and hooks.
 	# config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-	config.omniauth :facebook, "385390654848598", "2d253438162756df7334c8eb9fb656a5"
-
+	config.omniauth :facebook, "385390654848598", "2d253438162756df7334c8eb9fb656a5",
+	    {:scope => 'email, offline_access', :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}} 
+	
 	# ==> Warden configuration
 	# If you want to use other strategies, that are not supported by Devise, or
 	# change the failure app, you can configure them inside the config.warden block.
