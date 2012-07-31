7a347f2b97e3f7080285eccc462306384d780706
Edward Elliott <edelliott@nc.rr.com>
2012-07-31 10:43:44 -0400

    Fixing Facebook authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,4 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
 	# Include default devise modules. Others available are:
 	# :token_authenticatable, :confirmable,
 	# :lockable, :timeoutable and :omniauthable
@@ -17,6 +9,7 @@ class User < ActiveRecord::Base
 	# Setup accessible (or protected) attributes for your model
 	attr_accessible :email, :password, :password_confirmation, :remember_me,
 	                :provider, :uid
+
 	# attr_accessible :title, :body
 
 	def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 BuyStandConsumer::Application.routes.draw do
-  devise_for :users
-
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
 	root :to => "home#index"
